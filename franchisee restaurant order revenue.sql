b0ef1dda883ebb7ad0e8754c52b3e139382223a4
pungaa <pungaa@gmail.com>
2021-07-27 20:04:46 -0700

    Update franchisee restaurant order revenue.sql

--- franchisee restaurant order revenue.sql
+++ franchisee restaurant order revenue.sql
@@ -6,8 +6,8 @@ SELECT "Menu Item",
        sum("seat_cnt") AS "Total Seats",
        sum("AMOUNT") AS "Revenue"
 FROM
-  ( SELECT "oracle"."RDSORACLEFORPRESTO"."TLOG_PRODUCT_PLUNAMES"."NAME" AS "Menu Item",
-           "oracle"."RDSORACLEFORPRESTO"."TLOG_PRODUCT_PLUNAMES"."AMOUNT" AS "AMOUNT",
+  ( SELECT "Menu Items"."NAME" AS "Menu Item",
+           "Menu Items"."AMOUNT" AS "AMOUNT",
            "mysql"."promethium"."restaurant_orders"."created_dt" AS "created_dt",
            "mysql"."promethium"."restaurant_orders"."rest_nm" AS "rest_nm",
            "mysql"."promethium"."restaurant_orders"."city_nm" AS "City",
@@ -20,8 +20,8 @@ FROM
            "mysql"."promethium"."restaurant_orders"."royalty_pct" AS "royalty_pct",
            "mysql"."promethium"."restaurant_orders"."fran_rollup_nm" AS "Franchisee Parent",
            "mysql"."promethium"."restaurant_orders"."home_dlvry" AS "home_dlvry"
-   FROM "oracle"."RDSORACLEFORPRESTO"."TLOG_PRODUCT_PLUNAMES"
-   LEFT OUTER JOIN "mysql"."promethium"."restaurant_orders" ON ( "oracle"."RDSORACLEFORPRESTO"."TLOG_PRODUCT_PLUNAMES"."REST_KEY" = "mysql"."promethium"."restaurant_orders"."rest_key" ) )
+   FROM "oracle"."RDSORACLEFORPRESTO"."TLOG_PRODUCT_PLUNAMES" as "Menu Items"
+   LEFT OUTER JOIN "mysql"."promethium"."restaurant_orders" ON ( "Menu Items"."REST_KEY" = "mysql"."promethium"."restaurant_orders"."rest_key" ) )
 GROUP BY "Menu Item",
          "city",
          "State",

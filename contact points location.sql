0944e78d76d0e55f2a8f785fb718acca5b0123a4
pungaa <pungaa@gmail.com>
2021-08-12 22:22:00 -0700

    fixed sql alias

--- contact points location.sql
+++ contact points location.sql
@@ -1,19 +1,20 @@
-SELECT ps.party_id AS "Party Id",
-       ca.cust_account_id AS "Account Id",
-       ca.account_number AS "Account Number",
-       ca.status AS "Account Status",
-       cp.contact_point_id AS "Id",
-       cp.contact_point_type AS "Type",
-       cp.owner_table_name AS "Owner Table Name",
-       cp.status AS "Status",
-       su.site_use_code AS "Site Use Code",
-       su.status AS "Site Use Status",
-       su.location AS "Site Use Location"
-FROM "bigquery"."atd_dlk_ebs_data"."ar_hz_contact_points_gg" cp
-LEFT JOIN "bigquery"."atd_dlk_ebs_data"."ar_hz_party_sites_gg" ps ON cp.owner_table_id = ps.party_site_id
-LEFT JOIN "bigquery"."atd_dlk_ebs_data"."ar_hz_cust_acct_sites_all_gg" sa ON owner_table_id = sa.party_site_id
-LEFT JOIN "bigquery"."atd_dlk_ebs_data"."ar_hz_cust_accounts_gg" ca ON sa.cust_account_id = ca.cust_account_id
-LEFT JOIN "bigquery"."atd_dlk_ebs_data"."ar_hz_cust_site_uses_all_gg" su ON sa.cust_acct_site_id = su.cust_acct_site_id
+SELECT 
+       site_uses.status AS "Site Use Status",
+       site_uses.location AS "Site Use Location",
+       contact_points.status AS "Contact Point Status",
+       site_uses.site_use_code AS "Site Use Code",
+       party_sites.party_id AS "Party Id",
+       cust_accounts.cust_account_id AS "Account Id",
+       cust_accounts.account_number AS "Account Number",
+       cust_accounts.status AS "Account Status",
+       contact_points.contact_point_id AS "Id",
+       contact_points.contact_point_type AS "Type",
+       contact_points.owner_table_name AS "Owner Table Name"
+FROM "bigquery"."atd_dlk_ebs_data"."ar_hz_contact_points_gg" contact_points
+LEFT JOIN "bigquery"."atd_dlk_ebs_data"."ar_hz_party_sites_gg" party_sites ON contact_points.owner_table_id = party_sites.party_site_id
+LEFT JOIN "bigquery"."atd_dlk_ebs_data"."ar_hz_cust_acct_sites_all_gg" sites_all ON owner_table_id = sites_all.party_site_id
+LEFT JOIN "bigquery"."atd_dlk_ebs_data"."ar_hz_cust_accounts_gg" cust_accounts ON sites_all.cust_account_id = cust_accounts.cust_account_id
+LEFT JOIN "bigquery"."atd_dlk_ebs_data"."ar_hz_cust_site_uses_all_gg" site_uses ON sites_all.cust_acct_site_id = site_uses.cust_acct_site_id
 WHERE OWNER_TABLE_NAME = 'HZ_PARTY_SITES'
   AND LOCATION IS NOT NULL
 LIMIT 100
